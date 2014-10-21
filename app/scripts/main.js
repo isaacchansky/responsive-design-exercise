ed803678c08265bdc6a35d1599e0b11e8e963649
Isaac Chansky <ichansky@gmail.com>
2014-10-21 14:40:27 -0400

    set initial lead section & js to ensure it covers page on resize

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+function setLeadContainerSize() {
+    'use strict';
+    $('[role="lead-container"]').css({
+        minWidth: $(window).width()+'px',
+        minHeight: $(window).height()+'px'
+    });
+}
+
+
+$(document).ready(function(){
+    'use strict';
+    // initialize
+    setLeadContainerSize();
+
+    // Set event handlers
+    $(window).resize(setLeadContainerSize);
+});
